2f99db292ac6ad52edf6ea147972ac466ceb2bf6
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2022-01-22 10:38:38 -0600

    Added requirements.txt

--- ecuflash/settings.py
+++ ecuflash/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-23v-aqj53psk2ys5!%r_84oi_d-47gc@yl^@9989is0u1xn7xq'
+SECRET_KEY = environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
